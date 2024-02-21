f26c21e3bb4c80a969a88f6786772cec8cbbad62
paran-oid <azizhmid10000@gmail.com>
2024-02-21 11:46:37 +0100

    Reinstalled Venv and pip list

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\azizh\OneDrive\Documents\Portfolio\Programming\Projects\Learning Projects\facebook\venv
+set VIRTUAL_ENV=C:\Users\azizh\OneDrive\Documents\Portfolio\Programming\Projects\Learning Projects\facebook-new-edition\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
